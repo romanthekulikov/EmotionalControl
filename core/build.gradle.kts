ceb064b299d33b2c633495cb7b810f49319f3b5d
romanthekulikov <97156553+romanthekulikov@users.noreply.github.com>
2025-11-30 17:44:08 +0300

    Revert "Add router"

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "ru.kulikov.emotionalcontrol"
+    namespace = "ru.kulikov.core"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "ru.kulikov.emotionalcontrol"
         minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,11 +37,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(project(":feature:splash"))
 }
