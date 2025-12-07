85957f831146cda78975aeda3c27fda0acc236ba
Roman Kulikov <gadamob@gmail.com>
2025-12-07 18:11:58 +0300

    Add tests

--- feature/main/src/main/java/com/example/main/data/MainRepositoryImpl.kt
+++ feature/main/src/main/java/com/example/main/data/MainRepositoryImpl.kt
@@ -3,9 +3,10 @@ package com.example.main.data
 import com.example.main.domain.MainRepository
 import com.roman_kulikov.tools.Result
 import ru.kulikov.core.utils.data.Indicator
+import ru.kulikov.core.utils.domain.AppSharedPreferences
 import javax.inject.Inject
 
-class MainRepositoryImpl @Inject constructor() : MainRepository {
+class MainRepositoryImpl @Inject constructor(sharedPreferences: AppSharedPreferences) : MainRepository {
     override fun getUserIndicators(): Result<List<Indicator>> {
         TODO("Not yet implemented")
     }
