83559858df68fdc9717a59c7a52c5f2c8e6ee67d
Roman Kulikov <gadamob@gmail.com>
2025-12-08 00:00:41 +0300

    Add profile bl impl

--- core/src/main/java/ru/kulikov/core/utils/data/models/UserModel.kt
+++ core/src/main/java/ru/kulikov/core/utils/data/models/UserModel.kt
@@ -4,6 +4,5 @@ import ru.kulikov.core.utils.domain.entities.User
 
 data class UserModel(
     override val userId: Int,
-    override val avatarUrl: String,
     override val name: String,
 ) : User
--- feature/profile/src/main/java/com/example/profile/domain/use_cases/SaveUserUc.kt
+++ feature/profile/src/main/java/com/example/profile/domain/use_cases/SaveUserUc.kt
@@ -1,7 +1,8 @@
 package com.example.profile.domain.use_cases
 
 import com.roman_kulikov.tools.Result
+import ru.kulikov.core.utils.domain.entities.User
 
 interface SaveUserUc {
-    suspend operator fun invoke(): Result<Boolean>
+    suspend operator fun invoke(user: User): Result<Boolean>
 }
